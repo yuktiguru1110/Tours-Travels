d25f92528137dc45138915425111cd5e7c6cafbc
unknown <Yukti.Guru@lntinfotech.com>
2020-09-28 09:11:10 +0530

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,9 +5,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   constructor() { }
+  //  myFunction() {
+  //   var dots = document.getElementById("dots");
+  //   var moreText = document.getElementById("more");
+  //   var btnText = document.getElementById("myBtn");
+  
+  //   if (dots.style.display === "none") {
+  //     dots.style.display = "inline";
+  //     btnText.innerHTML = "Read more"; 
+  //     moreText.style.display = "none";
+  //   } else {
+  //     dots.style.display = "none";
+  //     btnText.innerHTML = "Read less"; 
+  //     moreText.style.display = "inline";
+  //   }
+  // }
 
   ngOnInit(): void {
   }
